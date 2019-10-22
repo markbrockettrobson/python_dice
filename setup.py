a823b6501fc6361b04c3d9a2d50255d1b89ed7a6
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-10-23 00:26:15 +1100

    formatter

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
     name="python_dice",
@@ -9,27 +9,22 @@ setuptools.setup(
     author="Mark Brockett Robson",
     author_email="mark.brockett.robson@gmail.com",
     description="a statistical dice library for python",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     url="https://github.com/markbrockettrobson/python_dice",
     packages=[
-        'python_dice',
-        'python_dice.interface',
-        'python_dice.interface.python_dice_expression',
-        'python_dice.interface.python_dice_syntax',
-        'python_dice.src',
-        'python_dice.src.python_dice_expression',
-        'python_dice.src.python_dice_syntax'
+        "python_dice",
+        "python_dice.interface",
+        "python_dice.interface.python_dice_expression",
+        "python_dice.interface.python_dice_syntax",
+        "python_dice.src",
+        "python_dice.src.python_dice_expression",
+        "python_dice.src.python_dice_syntax",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'matplotlib',
-        'rply',
-        'networkx',
-        'numpy'
-    ],
-    python_requires='>=3.6',
+    install_requires=["matplotlib", "rply", "networkx", "numpy"],
+    python_requires=">=3.6",
 )
