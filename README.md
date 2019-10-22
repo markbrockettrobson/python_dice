e851118eb2fea012ee69ad5b4e1759e687ea7b9e
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-10-23 00:11:48 +1100

    turned into a pip package

--- README.md
+++ README.md
@@ -5,3 +5,72 @@
 ![MIT](https://black.readthedocs.io/en/stable/_static/license.svg)
 
 a statistical dice library for python
+
+## to install
+~~~
+python -m pip install python_dice
+~~~
+
+## source at 
+https://github.com/markbrockettrobson/python_dice
+
+##Usage
+
+~~~
+from python_dice import PythonDiceInterpreter
+
+interpreter = PythonDiceInterpreter()
+program = [
+    "VAR six_sided_dice = 1d6",
+    "VAR two_six_sided_dice = 2d6",
+    "VAR odds_of_snake_eyes = ( two_six_sided_dice == 2 ) ",
+]
+roll = interpreter.roll(program).get_constant("six_sided_dice")
+print(roll)
+
+> 3
+~~~
+
+~~~
+interpreter = python_dice_interpreter.PythonDiceInterpreter()
+        program = [
+            "VAR save_roll = 1d20 + 8",
+            "VAR burning_arch_damage = 9d6 + 9",
+            "VAR pass_save = ( save_roll >= 19 ) ",
+            "VAR damage_half_on_save = burning_arch_damage // (pass_save + 1)",
+        ]
+        interpreter.get_probability_distribution(program).get_var("damage_half_on_save").show_histogram()
+
+~~~
+![image of 9d6 add 9 half round up if 1d20 add 8 greater than 19](https://raw.githubusercontent.com/markbrockettrobson/python_dice/master/images/level_nine_burning_arc.png)
+
+# Syntax
+
+Set a var
+~~~
+VAR lower_case_name = 1
+VAR name = 4d20
+~~~
+add, subtract, multiply, integer division
+~~~
+VAR name = 1 + 2d3 - 3 * 4d2 // 5
+~~~
+parenthesis
+~~~
+VAR out = 3 * ( 1 + 1d4 )
+~~~
+binary operator
+~~~
+VAR a = 1d4 == 1
+VAR b = 1d4 != 1
+VAR c = 1d4 <= 2
+VAR d = 1d4 < 3
+VAR e = 1d4 >= 2
+VAR f = 1d4 >= 2
+VAR g = (1d4 >= 2) AND !(1d20 == 2)
+VAR h = (1d4 >= 2) OR !(1d20 == 2)
+~~~
+abs
+~~~
+var abs = ABS( 1d6 - 1d6 )
+~~~
---
+++ init.py
@@ -0,0 +1,50 @@
+# import python_dice.interface.python_dice_expression.i_dice_expression
+# import python_dice.interface.python_dice_syntax.i_dice_syntax
+# import python_dice.interface.i_python_dice_parser
+# import python_dice.interface.i_python_dice_interpreter
+# import python_dice.interface.i_probability_state
+# import python_dice.interface.i_probability_distribution
+# import python_dice.interface.i_python_dice_lexer
+#
+# import python_dice.src.python_dice_syntax.assignment_syntax
+# import python_dice.src.python_dice_syntax.var_syntax
+# import python_dice.src.python_dice_syntax.name_syntax
+# import python_dice.src.python_dice_syntax.abs_syntax
+# import python_dice.src.python_dice_syntax.comma_syntax
+# import python_dice.src.python_dice_syntax.min_max_syntax
+# import python_dice.src.python_dice_syntax.binary_operator_syntax
+# import python_dice.src.python_dice_syntax.not_syntax
+# import python_dice.src.python_dice_syntax.constant_binary_syntax
+# import python_dice.src.python_dice_syntax.close_parenthesis_syntax
+# import python_dice.src.python_dice_syntax.open_parenthesis_syntax
+# import python_dice.src.python_dice_syntax.constant_integer_syntax
+# import python_dice.src.python_dice_syntax.add_syntax
+# import python_dice.src.python_dice_syntax.dice_syntax
+# import python_dice.src.python_dice_syntax.integer_division_syntax
+# import python_dice.src.python_dice_syntax.multiply_syntax
+# import python_dice.src.python_dice_syntax.subtract_syntax
+#
+# import python_dice.src.python_dice_expression.get_var_expression
+# import python_dice.src.python_dice_expression.var_assignment_expression
+# import python_dice.src.python_dice_expression.abs_expression
+# import python_dice.src.python_dice_expression.minmax_expression
+# import python_dice.src.python_dice_expression.binary_operator_expression
+# import python_dice.src.python_dice_expression.not_expression
+# import python_dice.src.python_dice_expression.constant_binary_expression
+# import python_dice.src.python_dice_expression.parentheses_enclosed_expression
+# import python_dice.src.python_dice_expression.subtract_expression
+# import python_dice.src.python_dice_expression.multiply_expression
+# import python_dice.src.python_dice_expression.integer_division_expression
+# import python_dice.src.python_dice_expression.dice_expression
+# import python_dice.src.python_dice_expression.constant_integer_expression
+# import python_dice.src.python_dice_expression.add_expression
+#
+# import python_dice.src.python_dice_interpreter
+# import python_dice.src.probability_state
+# import python_dice.src.python_dice_parser
+# import python_dice.src.probability_distribution
+# import python_dice.src.python_dice_lexer
+# import python_dice.src.python_dice_syntax_objects
+#
+#
+#
