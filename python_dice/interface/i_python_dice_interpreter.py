df41b03de6e15c81353f6dded90cb5be9d6304d3
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-12-23 14:46:11 +1100

    Renamed faction and added Distribution collection from interpreter

--- python_dice/interface/i_python_dice_interpreter.py
+++ python_dice/interface/i_python_dice_interpreter.py
@@ -2,6 +2,7 @@ import abc
 import typing
 
 import PIL.Image as Image
+import python_dice.interface.i_probability_distribution as i_probability_distribution
 
 
 class IPythonDiceInterpreter(abc.ABC):
@@ -27,9 +28,18 @@ class IPythonDiceInterpreter(abc.ABC):
         """
 
     @abc.abstractmethod
-    def get_probability_distributions(
+    def get_probability_distributions_dict(
         self, input_text: typing.List[str]
     ) -> typing.Dict[str, typing.Dict[int, float]]:
+        """
+
+        :return: output of state of the pydice program
+        """
+
+    @abc.abstractmethod
+    def get_probability_distributions(
+        self, input_text: typing.List[str]
+    ) -> typing.Dict[str, i_probability_distribution.IProbabilityDistribution]:
         """
 
         :return: output of state of the pydice program
